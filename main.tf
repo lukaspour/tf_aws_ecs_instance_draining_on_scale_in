667e76098445e368589daca4221b2d0f514076c8
Lukas Pour <lukaspour@gmail.com>
2019-07-22 15:13:08 +0200

    Moved to tf12, add descriptions, adjust to common shared tf from community

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ data "aws_iam_policy_document" "auto_scaling_notification_access" {
 
 resource "aws_iam_role_policy" "asg_notification_sns" {
   name   = "${aws_iam_role.sns.name}-asg-notification-policy"
-  role   = "${aws_iam_role.sns.id}"
-  policy = "${data.aws_iam_policy_document.auto_scaling_notification_access.json}"
+  role   = aws_iam_role.sns.id
+  policy = data.aws_iam_policy_document.auto_scaling_notification_access.json
 }
 
 resource "aws_iam_role" "lambda" {
@@ -93,14 +93,14 @@ data "aws_iam_policy_document" "lambda" {
 
 resource "aws_iam_role_policy" "lambda" {
   name   = "${aws_iam_role.lambda.name}-policy"
-  role   = "${aws_iam_role.lambda.id}"
-  policy = "${data.aws_iam_policy_document.lambda.json}"
+  role   = aws_iam_role.lambda.id
+  policy = data.aws_iam_policy_document.lambda.json
 }
 
 resource "aws_iam_role_policy" "asg_notification_lambda" {
   name   = "${aws_iam_role.lambda.name}-asg-notification-policy"
-  role   = "${aws_iam_role.lambda.id}"
-  policy = "${data.aws_iam_policy_document.auto_scaling_notification_access.json}"
+  role   = aws_iam_role.lambda.id
+  policy = data.aws_iam_policy_document.auto_scaling_notification_access.json
 }
 
 data "archive_file" "index" {
@@ -112,18 +112,18 @@ data "archive_file" "index" {
 resource "aws_lambda_function" "lambda" {
   runtime       = "python3.6"
   filename      = "${path.module}/files/index.zip"
-  function_name = "${substr(var.autoscaling_group_name,0,min(64, length(var.autoscaling_group_name)))}"
-  role          = "${aws_iam_role.lambda.arn}"
+  function_name = "${substr(var.autoscaling_group_name, 0, min(64, length(var.autoscaling_group_name)))}"
+  role          = aws_iam_role.lambda.arn
   handler       = "index.lambda_handler"
   timeout       = "${var.function_sleep_time * 2}"
 
-  source_code_hash = "${data.archive_file.index.output_base64sha256}"
+  source_code_hash = data.archive_file.index.output_base64sha256
 
   environment {
     variables = {
-      REGION       = "${var.region}"
-      CLUSTER_NAME = "${var.cluster_name}"
-      SLEEP_TIME   = "${var.function_sleep_time}"
+      REGION       = var.region
+      CLUSTER_NAME = var.cluster_name
+      SLEEP_TIME   = var.function_sleep_time
     }
   }
 
@@ -136,10 +136,10 @@ resource "aws_lambda_function" "lambda" {
 
 resource "aws_lambda_permission" "sns" {
   statement_id  = "AllowExecutionFromSNS"
-  function_name = "${aws_lambda_function.lambda.arn}"
+  function_name = aws_lambda_function.lambda.arn
   action        = "lambda:InvokeFunction"
   principal     = "sns.amazonaws.com"
-  source_arn    = "${aws_sns_topic.asg_sns.arn}"
+  source_arn    = aws_sns_topic.asg_sns.arn
 }
 
 resource "aws_sns_topic" "asg_sns" {
@@ -147,18 +147,18 @@ resource "aws_sns_topic" "asg_sns" {
 }
 
 resource "aws_sns_topic_subscription" "asg_sns" {
-  topic_arn = "${aws_sns_topic.asg_sns.arn}"
+  topic_arn = aws_sns_topic.asg_sns.arn
   protocol  = "lambda"
-  endpoint  = "${aws_lambda_function.lambda.arn}"
+  endpoint  = aws_lambda_function.lambda.arn
 }
 
 resource "aws_autoscaling_lifecycle_hook" "terminate" {
-  count                   = "${var.lambda_enabled}"
+  count                   = var.lambda_enabled ? 1 : 0
   name                    = "${var.autoscaling_group_name}-terminate-hook"
-  autoscaling_group_name  = "${var.autoscaling_group_name}"
-  default_result          = "${var.hook_default_result}"
-  heartbeat_timeout       = "${var.hook_heartbeat_timeout}"
+  autoscaling_group_name  = var.autoscaling_group_name
+  default_result          = var.hook_default_result
+  heartbeat_timeout       = var.hook_heartbeat_timeout
   lifecycle_transition    = "autoscaling:EC2_INSTANCE_TERMINATING"
-  notification_target_arn = "${aws_sns_topic.asg_sns.arn}"
-  role_arn                = "${aws_iam_role.sns.arn}"
+  notification_target_arn = aws_sns_topic.asg_sns.arn
+  role_arn                = aws_iam_role.sns.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,17 @@
-variable "region" {}
+variable "region" {
+  description = "Region"
+  type        = string
+}
 
-variable "cluster_name" {}
+variable "cluster_name" {
+  description = "Name of cluster to be used for draining during scaling"
+  type        = string
+}
 
-variable "autoscaling_group_name" {}
+variable "autoscaling_group_name" {
+  description = "Name of autoscaling group to be used for draning during scaling"
+  type        = string
+}
 
 variable "function_sleep_time" {
   description = "Number of seconds the function should sleep before checking ECS Instance Task Count again"
@@ -10,13 +19,19 @@ variable "function_sleep_time" {
 }
 
 variable "lambda_enabled" {
-  default = true
+  description = "For some reasons, you could be interested in just uploading the lambda, not making it work"
+  type        = bool
+  default     = true
 }
 
 variable "hook_heartbeat_timeout" {
-  default = 900
+  description = "Timeout of the heartbeat during the scaling process"
+  type        = number
+  default     = 900
 }
 
 variable "hook_default_result" {
-  default = "ABANDON"
+  description = "Default action to apply if the hook timeouts"
+  type        = string
+  default     = "ABANDON"
 }
